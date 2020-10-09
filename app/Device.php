9bbdbcfae4c63b2bc94498db7ef19b68a66067b7
Ruslan <kambarov.rs@gmail.com>
2020-10-09 15:02:04 +0600

    device total income and consume methods

--- app/Device.php
+++ app/Device.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use DB;
 use Cloud;
 use App\Consumption;
 use Illuminate\Database\Eloquent\Model;
@@ -42,12 +43,22 @@ class Device extends Model
         return $this->required_p ?? DB::table('app_settings')->find(6)->value();
     }
 
+    public function getObjectTotalIncome(){
+        $incomed = DB::select(DB::raw("SELECT SUM(income) as incomed FROM consumption WHERE object_id = $this->owen_id"))[0]->incomed ?? null;
+        return $incomed;        
+    }
+
+    public function getObjectTotalConsume(){
+        $consumed = DB::select(DB::raw("SELECT SUM(consumption) as consumed FROM consumption WHERE object_id = $this->owen_id"))[0]->consumed ?? null;
+        return $consumed;        
+    }
+
     public function getConsumption(){       
         $consumption = Consumption::where('object_id', $this->owen_id)->latest()->first();
-        $this->device_id 	 = $this->owen_id;
-        unset($this->owen_id, $this->id);
-        $this->income 		 = $consumption->income ?? 0;
-        $this->consumption   = $consumption->consumption ?? 0;
+        $this->device_id 	 = $this->owen_id;        
+        $this->income 		 = $this->getObjectTotalIncome() ?? 0;
+        $this->consumption   = $this->getObjectTotalConsume() ?? 0;
+        unset($this->owen_id, $this->id);        
         $this->balance 		 = $consumption->balance ?? 0;
         $this->status 		 = $this->balance > $this->consumption * 14;
         return $this;
